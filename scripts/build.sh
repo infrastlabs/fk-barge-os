97807786c6604250eddbf603a239ae381fc9aacc
sam#gemmi-win10 <sam@china.gz>
2025-01-13 21:56:52 +0800

    scripts/build.sh mkdir -p /build/patches/linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,9 @@ tar -zxf /output_brdata.tar.gz -C /tmp
 \cp -a /tmp/brdata/build_buildroot_dl/* /build/buildroot/dl/
 ls -lh /build/buildroot/dl/; du -sh /build/buildroot/dl
 
+# 
+mkdir -p /build/patches/linux
+
 # Add the basics startup scripts
 cp -f ${OVERLAY}/etc/init.d/* package/initscripts/init.d/
 install -C -m 0755 package/initscripts/init.d/* ${OVERLAY}/etc/init.d/
