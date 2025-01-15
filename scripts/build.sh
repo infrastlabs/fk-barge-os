d4a829a59230b642fd8798b5fc5b542dc4a30984
sam <huapox@126.com>
2025-01-15 14:40:56 +0800

    scripts/build.sh pre-down util-linux-2.35.1.tar.xz; # notes: clear old cache --
    ##4.9M ref _ref\config\BR_2002_K540.md
    
    scripts/build.sh fix curl -k -fSL -o /build/buildroot/dl/util-linux/util-linux-2.35.1.tar.xz
    
    scripts/build.sh # notes: clear old cache
    # \cp -a /tmp/brdata/build_buildroot_dl/* /build/buildroot/dl/
    
    scripts/build.sh # notes: clear old cache ##700M> 370M;

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,16 @@ set -e
 
 # brdata:src,-cache
 tar -zxf /output_brdata.tar.gz -C /tmp
+# notes: clear old cache ##700M> 370M;
 \cp -a /tmp/brdata/build_buildroot_dl/* /build/buildroot/dl/
 ls -lh /build/buildroot/dl/; du -sh /build/buildroot/dl
 
 # 
 mkdir -p /build/patches/linux
+# mkdir -p /build/buildroot/dl/util-linux
+# ##4.9M ref _ref\config\BR_2002_K540.md
+# curl -k -fSL -o /build/buildroot/dl/util-linux/util-linux-2.35.1.tar.xz \
+#   https://gitee.com/g-system/fk-barge-os/releases/download/v2305-ke-start/util-linux-2.35.1.tar.xz
 
 # Add the basics startup scripts
 cp -f ${OVERLAY}/etc/init.d/* package/initscripts/init.d/
